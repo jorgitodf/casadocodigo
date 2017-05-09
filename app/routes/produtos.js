46ed8c469fca3b78332141b696e1b3f14887e01e
jorgitodf <jspaiva.1977@gmail.com>
2017-05-09 16:27:13 -0300

    COESÃO E ACOPLAMENTO, ORGANIZANDO O CÓDIGO

---
+++ app/routes/produtos.js
@@ -0,0 +1,12 @@
+module.exports = function(app) {
+    app.get('/produtos', function(req,res) {
+        var connection = app.infra.connectionFactory();
+        var produtosBanco = new app.infra.ProdutosDAO(connection);
+
+        produtosBanco.lista(function(err, results) {
+            res.render('produtos/lista',{lista:results});
+        });
+
+        connection.end();
+    });
+}
